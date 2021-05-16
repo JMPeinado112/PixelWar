8fcff0f2278aec2b02836059030aaf8cae1e0361
JMPeinado112 <72929210+JMPeinado112@users.noreply.github.com>
2021-05-16 08:08:18 +0200

    Update Equipo.java

--- PixelWar/src/Equipo.java
+++ PixelWar/src/Equipo.java
@@ -1,18 +1,24 @@
 public class Equipo{
 
 	String Nombre;
-	static int hp, misil;
+	int hp, misil;
 	String tipo;
 	int misila, misild;
+	int contador = 0;
 	
 	public Equipo(String tipo, String nom){
 		
 		this.Nombre=nom;
 		this.tipo = tipo;
-		this.hp=gethptipo(tipo);
-		this.misil=getmisilestipo(tipo);
+		
+			this.hp=gethptipo(tipo);
+			this.misil=getmisiltipo(tipo);
+		
+		
+		contador++;
 	}
-	public static int gethptipo(String tipo) {
+	//se passa la vida de cada tipo a la batalla
+	public int gethptipo(String tipo) {
 		if(tipo.equals("Stickman")) {
 			hp = 200;
 		}
@@ -46,42 +52,156 @@ public class Equipo{
 		
 		return hp;
 	}
-	
-	public static int getmisilestipo(String tipo) {
+	//se passa los missiles de cada tipo a la batalla
+	public int getmisiltipo(String tipo) {
+		int misiles = 0;
 		if(tipo.equals("Stickman")) {
-			misil = 50;
+			misiles = 50;
 		}
 		if(tipo.equals("Link")) {
-			misil = 50;
+			misiles = 50;
 		}
 		if(tipo.equals("Link")) {
-			misil = 50;
+			misiles = 50;
 		}
 		if(tipo.equals("Sonic")) {
-			misil = 50;
+			misiles = 50;
 		}
 		if(tipo.equals("Ratchet & Clank")) {
-			misil = 50;
+			misiles = 50;
 		}
 		if(tipo.equals("Donkey Kong")) {
-			misil = 20;
+			misiles = 20;
 		}
 		if(tipo.equals("Clank")) {
-			misil = 20;
+			misiles = 20;
 		}
 		if(tipo.equals("Mario")) {
-			misil = 50;
+			misiles = 50;
 		}	
 		if(tipo.equals("Pikachu")) {
-			misil = 50;
+			misiles = 50;
 		}
 		if(tipo.equals("Sub-Zero")) {
-			misil = 50;
+			misiles = 50;
 		}
 		
-		return misil;
+		return misiles;
 	}
-	
+	public static int getmisilestipo(String tipo) {
+		int misiles = 0;
+		if(tipo.equals("Stickman")) {
+			misiles = 50;
+		}
+		if(tipo.equals("Link")) {
+			misiles = 50;
+		}
+		if(tipo.equals("Link")) {
+			misiles = 50;
+		}
+		if(tipo.equals("Sonic")) {
+			misiles = 50;
+		}
+		if(tipo.equals("Ratchet & Clank")) {
+			misiles = 50;
+		}
+		if(tipo.equals("Donkey Kong")) {
+			misiles = 20;
+		}
+		if(tipo.equals("Clank")) {
+			misiles = 20;
+		}
+		if(tipo.equals("Mario")) {
+			misiles = 50;
+		}	
+		if(tipo.equals("Pikachu")) {
+			misiles = 50;
+		}
+		if(tipo.equals("Sub-Zero")) {
+			misiles = 50;
+		}
+		
+		return misiles;
+	}
+	//se passa la respectiva imagen del jugador a la batalla
+	public static String foto(String tipo) {
+		String imagen = tipo;
+		
+		if(tipo.equals("Stickman")) {
+			imagen = "assets/personajes/Stickman.png";
+		}
+		if(tipo.equals("Link")) {
+			imagen = "assets/personajes/Link.png";
+		}
+		if(tipo.equals("Creeper")) {
+			imagen = "assets/personajes/Creeper.png";
+		}
+		if(tipo.equals("Sonic")) {
+			imagen = "assets/personajes/Sonic.png";
+		}
+		if(tipo.equals("Ratchet & Clank")) {
+			imagen = "assets/personajes/Ratchet and Clank.png";
+		}
+		if(tipo.equals("Donkey Kong")) {
+			imagen = "assets/personajes/DK.png";
+		}
+		if(tipo.equals("Clank")) {
+			imagen = "assets/personajes/clank.png";
+		}
+		if(tipo.equals("Mario")) {
+			imagen = "assets/personajes/Mario.png";
+		}
+		if(tipo.equals("Pikachu")) {
+			imagen = "assets/personajes/Pikachus.png";
+		}
+		if(tipo.equals("Sub-Zero")) {
+			imagen = "assets/personajes/Sub-zero.png";
+		}
+		if(tipo.equals("Goku")) {
+			imagen = "assets/personajes/Gokus.png";
+		}
+		
+		return imagen;
+	}
+	public static String cara(String tipo) {
+		String imagen = tipo;
+		
+		if(tipo.equals("Stickman")) {
+			imagen = "assets/caras/Stickman.png";
+		}
+		if(tipo.equals("Link")) {
+			imagen = "assets/caras/Link.png";
+		}
+		if(tipo.equals("Creeper")) {
+			imagen = "assets/caras/Creeper.png";
+		}
+		if(tipo.equals("Sonic")) {
+			imagen = "assets/caras/Sonic.png";
+		}
+		if(tipo.equals("Ratchet & Clank")) {
+			imagen = "assets/caras/Ratchet and Clank.png";
+		}
+		if(tipo.equals("Donkey Kong")) {
+			imagen = "assets/caras/Donkey Kong.png";
+		}
+		if(tipo.equals("Clank")) {
+			imagen = "assets/caras/clank.png";
+		}
+		if(tipo.equals("Mario")) {
+			imagen = "assets/caras/Mario.png";
+		}
+		if(tipo.equals("Pikachu")) {
+			imagen = "assets/caras/Pikachu.png";
+		}
+		if(tipo.equals("Sub-Zero")) {
+			imagen = "assets/caras/Sub-Zero.png";
+		}
+		if(tipo.equals("Goku")) {
+			imagen = "assets/caras/Goku.png";
+		}
+		
+		return imagen;
+	}
 	
 	
 	public int recibirmisiles(String nome, int cantidad, String nom, int hp, int misilesE, int misilesD) {
