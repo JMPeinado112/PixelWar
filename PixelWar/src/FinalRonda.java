78c40cbc0d5fc6fb067c19502408e3768479d10e
JMPeinado112 <72929210+JMPeinado112@users.noreply.github.com>
2021-05-16 08:07:55 +0200

    Update FinalRonda.java

--- PixelWar/src/FinalRonda.java
+++ PixelWar/src/FinalRonda.java
@@ -1,8 +1,9 @@
-
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
 import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -15,51 +16,53 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class FinalRonda extends JPanel implements ActionListener{
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JScrollPane scroll;
 	public static String text;
-	private JButton continuar;
+	public static int contador;
+	private static JButton continuar;
 	private int cantidad;
 	private String tipos[];
 	private String nom[];
-	static int contador = 1;
-
-	public FinalRonda(ArrayList resumen, int cantidad, String tipos[], String nom[]) {
-		this.cantidad = cantidad;
+	private JLabel rondas;
+	public FinalRonda(String toString, int cantidad, String tipos[], String nom[]) {this.cantidad = cantidad;
 		this.tipos = tipos;
 		this.nom = nom;
 		
 		setBounds(100, 100, 1100, 700);
 		setLayout(null);
-		continuar = new JButton("continuar");
-		continuar.setBounds(416, 409, 163, 76);
+		
+	
+		continuar = new JButton(new ImageIcon(new ImageIcon("assets/botones/frondab.png").getImage().getScaledInstance(160, 72, Image.SCALE_DEFAULT)));
+		continuar.setBounds(468, 539, 160, 76);
+		continuar.setPressedIcon(new ImageIcon(new ImageIcon("assets/botones/frondab1.png").getImage().getScaledInstance(160, 72, Image.SCALE_DEFAULT)));
+		continuar.setFocusable(true);
+		continuar.setBorder(null);
+		continuar.setFocusPainted(false);
+		continuar.setContentAreaFilled(false);
 		add(continuar);
 		continuar.addActionListener(this);
-	    	JScrollBar scrollBar = new JScrollBar();
-			scrollBar.setBounds(1044, 131, 40, 376);
-			  scroll = new JScrollPane(textField);
-		        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-
-			add(scrollBar);
-			
-			textField = new JTextField();
-			textField.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 15));
-			textField.setText(resumen.toString() + "ronda : " + contador);
-			
-			textField.setBounds(0, 131, 1044, 376);
-			textField.setEditable(false); // set textArea non-editable
+	  
+			JTextArea textArea = new JTextArea(Batalla.texto);
+			textArea.setForeground(new Color(51, 204, 102));
+			textArea.setEditable(false);
+			textArea.setOpaque(false);
+			textArea.setFont(new Font("Bahnschrift", Font.BOLD, 25));
+			textArea.setBounds(21, 106, 1063, 422);
+			add(textArea);
 		
-			textField.setColumns(10);
-			add(textField);
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(0, 0, 1084, 661);
-		add(lblNewLabel);
+			rondas = new JLabel(new ImageIcon(new ImageIcon("assets/botones/resumen.png").getImage().getScaledInstance(240,70, Image.SCALE_DEFAULT)));
+			rondas.setBounds(430, 11, 240, 70);
+			add(rondas);
+			
+			JLabel lblNewLabel = new JLabel(new ImageIcon(new ImageIcon("assets/fondos/fronda.png").getImage().getScaledInstance(1084,661, Image.SCALE_DEFAULT)));
+			lblNewLabel.setBounds(0, 0, 1084, 661);
+			add(lblNewLabel);
 	}
 
 	@Override
@@ -67,9 +70,11 @@ public class FinalRonda extends JPanel implements ActionListener{
 		if(e.getSource()==continuar) {
 			JFrame Marco = (JFrame)SwingUtilities.getWindowAncestor(this);
 			Marco.remove(this);
-			Marco.getContentPane().add(new Batalla(this.cantidad, this.tipos, this.nom));
+			Marco.getContentPane().add(new Batalla(this.cantidad, this.tipos, this.nom, Eleccion.equipos));
 			Marco.setVisible(true);
 		}
 		
 	}
+	
+	
 }
